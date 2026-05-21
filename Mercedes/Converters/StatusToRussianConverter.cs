4c54078de1b2bbcd64b0857314e0b7c71717ded0
artxmix <artem.artusevskij01@gmail.com>
2026-05-21 20:53:24 +0300

    save

---
+++ Mercedes/Converters/StatusToRussianConverter.cs
@@ -0,0 +1,29 @@
+﻿using Mercedes.Data.Enums;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mercedes.Converters;
+
+public class StatusToRussianConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is SaleStatus status)
+        {
+            return status switch
+            {
+                SaleStatus.Pending => "Ожидает",
+                SaleStatus.Completed => "Выполнено",
+                SaleStatus.Cancelled => "Отменено",
+                _ => status.ToString()
+            };
+        }
+
+        return Binding.DoNothing;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
